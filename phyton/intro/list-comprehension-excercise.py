5f380f6a477767000598ef454b927c8f57c1c0f3
Iten Thomas ( U118822 ) <thomas.iten@mobi.ch>
2019-05-23 09:31:09 +0200

    phyton advanced stuff added.

---
+++ phyton/intro/list-comprehension-excercise.py
@@ -0,0 +1,10 @@
+
+# Exercise
+# Using a list comprehension, create a new list called "newlist" out of the list "numbers",
+# which contains only the positive numbers from the list, as integers.
+
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+newlist = [n for n in numbers if n>0]
+print(newlist)
+
+
